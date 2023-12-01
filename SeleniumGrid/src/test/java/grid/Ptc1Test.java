13b04a96b6cd5b9808fe8b88ddd20ace4d3854cf
sanjaySDET <sanjaybabusdet@gmail.com>
2023-12-01 15:01:50 +0530

    1c

---
+++ SeleniumGrid/src/test/java/grid/Ptc1Test.java
@@ -0,0 +1,12 @@
+package grid;
+
+import org.testng.annotations.Test;
+
+public class Ptc1Test {
+
+	@Test
+	public void m1Test()
+	{
+	System.out.println("hi");	
+	}
+}
---
+++ SeleniumGrid/src/test/java/grid/Ptc2Test.java
@@ -0,0 +1,12 @@
+package grid;
+
+import org.testng.annotations.Test;
+
+public class Ptc2Test {
+	
+@Test	
+public void m2Test()
+{
+System.out.println("hello");	
+}
+}
